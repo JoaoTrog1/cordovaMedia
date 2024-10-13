e6d6576c0caa2bd65c9a09f766e18f37b150b66a
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-10-12 21:38:42 -0300

    Finalizando layout

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,29 +1,107 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+let media; // essa variavel salva o objeto media
+let intervalId;
+let isPaused = false;
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('deviceready', function () {
+    // Caminho do áudio
+    const audioFolder = cordova.file.applicationDirectory + 'www/audio/';
+    
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+    function listAudioFiles() {
+        const audioFiles = [
+            'audio.mp3',
+            'audio3.mp3'
+        ];
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+        audioFiles.forEach(fileName => {
+            const button = document.createElement('button');
+            button.innerText = fileName;
+            button.onclick = function () {
+                playAudio(audioFolder + fileName);
+            };
+            document.getElementById('audioList').appendChild(button);
+        });
+    }
+
+    
+    function playAudio(filePath) {
+        
+        if (media) {
+            media.stop(); 
+        }
+
+        media = new Media(filePath,
+            function () { console.log("Sucesso ao reproduzir o áudio"); },
+            function (err) { console.log("Erro: " + err); }
+        );
+        media.play();
+
+        
+            
+
+
+        intervalId = setInterval(() => {
+            getAudioInfo();
+            sta2 = document.getElementById('status2');
+        sta2.innerText = "Duração total: " + media.getDuration() + " segundos";
+        }, 10);
+        
+        
+        media.onStatusUpdate = function(status) {
+            if (status === Media.MEDIA_STOPPED) {
+                playAudio(filePath); 
+            }
+        };
+    }
+
+   
+    function pauseAudio() {
+        if (media) {
+            isPaused = true;
+            media.pause();
+        }
+    }
+
+    const pauseButton = document.createElement('button');
+    pauseButton.innerText = "Pausar Áudio";
+    pauseButton.onclick = pauseAudio;
+    document.body.appendChild(pauseButton);
+
+
+
+    function getAudioInfo() {
+        if (media) {
+            sta = document.getElementById('status');
+            
+            media.getCurrentPosition(
+                function (position) {
+                    if (position >= 0) {
+                        sta.innerText = "Tempo atual: " + position + " segundos";
+                    }
+                },
+                
+            );
+
+            
+        }
+    }
+
+     const resumeButton = document.createElement('button');
+     resumeButton.innerText = "Retomar Áudio";
+     resumeButton.onclick = function() {
+         if (media && isPaused) {
+             media.play(); 
+             isPaused = false; 
+           
+             intervalId = setInterval(() => {
+                 getAudioInfo();
+             }, 1000);
+         }
+     };
+     document.body.appendChild(resumeButton);
+ 
+
+
+    
+    listAudioFiles();
+}, false);
